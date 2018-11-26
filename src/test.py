d813c4ccb495aef189ab944939057913885dc91e
Linukas <linukaszkr@gmail.com>
2018-11-26 15:08:34 +0100

    utility and data set
    utilities developed.
    created the artist datasets

--- src/test.py
+++ src/test.py
@@ -9,12 +9,20 @@ import time
 
 #Give the path of the dataset
 pretime = time.time()
-pathOfDataset = 'C:/Users\linuk\Desktop\Staedel_Teilset/Objekte.xml'
+pathOfDataset = 'C:/Users\linuk\Desktop\Staedel/Objekte.xml'
+pathOfArtistSet = 'listAll.xml'
 tree = etree.parse(pathOfDataset)
 root = tree.getroot()
+treeArtist = etree.parse(pathOfArtistSet)
+rootArtist = treeArtist.getroot()
+# listing = ut.getAllArtist(root)
+# ut.create_xml(listing,'C:/Users\linuk\Desktop/listAll.xml')
+
 title, name, createtime = ut.get_start_info('464', root)
-print('\nTitle is: ' +title +'\n')
-print('Artist is: ' +name +'\n')
-print('Created time is: ' +createtime +'\n')
-print(ut.search_artist(name))
-print('\nDuration of the test: %.3f seconds'%(time.time()-pretime))
+print(ut.search_artist_xml(name,rootArtist))
+
+# print('\nTitle is: ' +title +'\n')
+# print('Artist is: ' +name +'\n')
+# print('Created time is: ' +createtime +'\n')
+# print(ut.search_artist_wiki(name))
+# print('\nDuration of the test: %.3f seconds'%(time.time()-pretime))
